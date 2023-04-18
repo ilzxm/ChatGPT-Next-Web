d1e8041559290f62292ac482e143aa8bebbd8199
ilzxm <ilzxm@outlook.com>
2023-04-18 10:13:01 +0800

    更改网页标题

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -167,9 +167,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>文心一言</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            通义千问——您的专属 ChatGPT.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "文心一言",
+  description: "通义千问",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "文心一言",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
