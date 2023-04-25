08e380d14ba549d79aada88d2de040aa46026e36
ilzxm <ilzxm@outlook.com>
2023-04-25 20:38:29 +0800

    更改网页标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -91,7 +91,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT</div>
-        <div className={styles["sidebar-sub-title"]}>张雪梅的 ChatGPT.</div>
+        <div className={styles["sidebar-sub-title"]}>张雪梅的ChatGPT.</div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
         </div>
